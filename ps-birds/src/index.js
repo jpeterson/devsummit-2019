58bb15c60755d8dcea2f4569ff45b4498b80b5e3
Joshua Peterson <jpeterson@esri.com>
2019-03-05 14:09:01 -0800

    prettier fixes

--- ps-birds/src/index.js
+++ ps-birds/src/index.js
@@ -9,8 +9,9 @@ import CalciteThemeProvider from 'calcite-react/CalciteThemeProvider';
 ReactDOM.render(
   <CalciteThemeProvider>
     <App />
-  </CalciteThemeProvider>
-  , document.getElementById('root'));
+  </CalciteThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
